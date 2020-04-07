e3a0abdc381931a1bff821f224dd5d5380b33605
DanielMagik <32642352+DanielMagik@users.noreply.github.com>
2020-04-07 12:40:47 +0200

    Dodano struktury tabel bazy danych

---
+++ imageDetect/Model/Photos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace imageDetect.Model
+{
+    public class Photos : Value
+    {
+        public byte[] Photo { get; set; }
+        public int FoundNumber { get; set; }
+        public int RealNumber { get; set; }
+        public int ObjectID { get; set; }
+        public virtual Statistics PhotoStatistics { get; set; }
+    }
+}
---
+++ imageDetect/Model/Statistics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace imageDetect.Model
+{
+    public class Statistics : Value
+    {
+        public int NumberOfSearches { get; set; }
+        public int ZeroObjects { get; set; }
+        public double AverageError { get; set; }
+        public virtual ICollection <Photos> RelatedPhotos { get; set; }
+    }
+}

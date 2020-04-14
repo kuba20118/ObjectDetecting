2b77e26c5c5befacd4b1c32f4dc404728834dc93
Krzysztof Kuczyński <krzykuc903@student.polsl.pl>
2020-04-14 18:02:18 +0200

    osobliwy hello world w reakcie

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import {Container, Row, Col} from 'react-bootstrap';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [titleStyle, setTitleStyle] = useState("");
+  return (    
+    
+    <Container>
+      <Row>
+        <p style={{font: titleStyle}}>CatFinder</p>
+      </Row>
+      
+      <Row>
+        <button onClick={()=>setTitleStyle("bold 30px Comic Sans MS, serif")}>
+          Comic Sans-inator
+        </button>
+      </Row>          
+         
+    </Container>
   );
 }
 

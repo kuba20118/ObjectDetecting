619a95ed4a18f450b452d55575b8921426625c3f
Kuba Sądel <speszal97@gmail.com>
2020-04-18 01:14:45 +0200

    added ImagesRepository and StatsRepository

---
+++ imageDetect/Data/Stats/IStatsRepository.cs
@@ -0,0 +1,9 @@
+using System.Threading.Tasks;
+
+namespace imageDetect.Data
+{
+    public interface IStatsRepository
+    {
+ 
+    }
+}
---
+++ imageDetect/Data/Stats/StatsRepository.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+using imageDetect;
+
+namespace imageDetect.Data
+{
+    public class StatsRepository : IStatsRepository
+    {
+        
+    }
+}

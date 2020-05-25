90e81eaa5890dfa25756c22f62b0b8470441d941
kuba20118 <kuba.pi1997@gmail.com>
2020-05-25 13:58:02 +0200

    travis v14

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,7 @@ echo "##"
 pwd
 ls
 echo "##"
-dotnet publish ./src/Detector.Api -c Release -o ./bin/Docker
+dotnet publish ./src/Detector.Api -c Release -o ./src/Detector.Api/bin/Docker
 echo "##"
 pwd
 ls ./bin/Docker

e249031581a8dbffd530fbc8d5e421d46b464cf5
kuba20118 <kuba.pi1997@gmail.com>
2020-05-25 14:08:35 +0200

    travis v15

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -12,24 +12,12 @@ case "$TRAVIS_BRANCH" in
     ;;    
 esac
 
+echo "1. log in"
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-echo "1. logged"
 cd ./src/Detector.Api
-echo "###"
-pwd
-ls 
-echo "###"
+echo "2. building"
 docker build -t detector.api:latest .
-echo "2. built"
-echo "###"
-pwd
-ls ./src/Detector.Api/bin/docker
-echo "###"
-echo "###"
-pwd
-ls
-echo "###"
-docker tag detector.api:latest $DOCKER_USERNAME/detector.api:latest
-echo "3. tagged"
+echo "3. tagging"
+docker tag detector.api:latest $DOCKER_USERNAME/detector:latest
+echo "4. pushing"
 docker push $DOCKER_USERNAME/detector.api:latest
-echo "4. pushed"
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-echo "##"
-pwd
-ls
-echo "##"
 dotnet publish ./src/Detector.Api -c Release -o ./src/Detector.Api/bin/Docker
-echo "##"
-pwd
-ls ./bin/Docker
-echo "##"
